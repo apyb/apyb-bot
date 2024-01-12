9f57a1d6e1d65cccb112dbf8d28a3ac6cb596c14
naanadr <fsandrade25@gmail.com>
2024-01-12 19:05:44 -0300

    Adiciona docstrings

--- apyb/email.py
+++ apyb/email.py
@@ -15,7 +15,21 @@ def smtp_connection(username: str, password: str):
     return connection
 
 
-def build_message(from_email: str, to_email: str, subject: str, html_body: str):
+def build_message(
+    from_email: str, to_email: str, subject: str, html_body: str
+) -> MIMEMultipart:
+    """
+    Builds an email message with the specified parameters.
+
+    Args:
+        from_email (str): The sender's email address.
+        to_email (str): The recipient's email address.
+        subject (str): The subject of the email.
+        html_body (str): The HTML body of the email.
+
+    Returns:
+        message (MIMEMultipart): The constructed email message.
+    """
     message = MIMEMultipart()
     message["From"] = from_email
     message["To"] = to_email
@@ -26,7 +40,24 @@ def build_message(from_email: str, to_email: str, subject: str, html_body: str):
     return message
 
 
-def send_email_as_no_reply(to_email: str, subject: str, body: str, smtp_password: str):
+def send_email_as_no_reply(
+    to_email: str,
+    subject: str,
+    body: str,
+    smtp_password: str,
+) -> None:
+    """
+    Sends an email as a no-reply email address.
+
+    Args:
+        to_email (str): The recipient's email address.
+        subject (str): The subject of the email.
+        body (str): The body of the email.
+        smtp_password (str): The password for the SMTP server.
+
+    Returns:
+        None
+    """
     message = build_message(EMAIL_NO_REPLY, to_email, subject, body)
 
     context = ssl.create_default_context()
--- apyb/github/client.py
+++ apyb/github/client.py
@@ -5,6 +5,15 @@ GITHUB_GRAPHQL_ENDPOINT = "https://api.github.com/graphql"
 
 
 def get_github_client(token: str) -> Client:
+    """
+    Returns a GitHub client object initialized with the provided token.
+
+    Args:
+        token (str): The GitHub access token.
+
+    Returns:
+        Client: The initialized GitHub client object.
+    """
     transport = HTTPXTransport(
         url=GITHUB_GRAPHQL_ENDPOINT,
         headers={
