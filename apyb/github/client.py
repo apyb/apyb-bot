4694c95bf06bf4e642d174f0ea1ae3b658753411
naanadr <fsandrade25@gmail.com>
2024-01-12 19:00:37 -0300

    Adiciona timeout igual a None

--- apyb/github/client.py
+++ apyb/github/client.py
@@ -4,11 +4,12 @@ from gql.transport.httpx import HTTPXTransport
 GITHUB_GRAPHQL_ENDPOINT = "https://api.github.com/graphql"
 
 
-def get_github_client(token: str):
+def get_github_client(token: str) -> Client:
     transport = HTTPXTransport(
         url=GITHUB_GRAPHQL_ENDPOINT,
         headers={
             "Authorization": f"Bearer {token}",
         },
+        timeout=None,
     )
     return Client(transport=transport, fetch_schema_from_transport=True)
